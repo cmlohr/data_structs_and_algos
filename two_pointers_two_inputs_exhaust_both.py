101f8cba970babb9cea40580170ce7354663dfee
cmlohr <cmlohr@outlook.com>
2023-08-16 11:38:28 -0500

    added 2 pointers 2 inputs exhast both

---
+++ two_pointers_two_inputs_exhaust_both.py
@@ -0,0 +1,19 @@
+def fn(arr1, arr2):
+    i = j = ans = 0
+
+    while i < len(arr1) and j < len(arr2):
+        #do some logic here
+        if CONDITION:
+            i
+        else:
+            j += 1
+
+    while i < len(arr1):
+        #do some logic here
+        i += 1
+
+    while j < len(arr2):
+        #do some logic here
+        j += 1
+
+    return ans
