1ddcfd833c718ad2f202fb489a94a4f4d1a4d058
cmlohr <cmlohr@outlook.com>
2023-08-16 11:41:12 -0500

    add aliding window

---
+++ sliding_window.py
@@ -0,0 +1,12 @@
+def fn(arr):
+    left = ans = curr = 0
+
+    for right in range(len(arr)):
+        # do logic here to add arr[right] to curr
+
+        while WINDOW_CONDITION_BROKEN:
+            # remove arr[left] from cure
+            left += 1
+
+        # update ans
+    return ans
